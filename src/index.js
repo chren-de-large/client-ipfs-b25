38874f37e519864159e1e0475b3e6ff454d67661
Peter Jensen <tuximail@gmail.com>
2018-11-03 23:53:47 +0100

    added cname record

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const ipfsOptions = {
   config: {
     Addresses: {
       Swarm: [
-        '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star',
+        '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star/'
         // Use local signal server
         // '/ip4/0.0.0.0/tcp/9090/wss/p2p-webrtc-star',
       ]
